23a04adb73ff6047263b99409d4ab03f0a4877e0
Ritesh Patel <129499361+riteshpatel-1884@users.noreply.github.com>
2025-05-30 18:09:36 +0530

    Update navbar.jsx

--- Redux Toolkit/navbar.jsx
+++ Redux Toolkit/navbar.jsx
@@ -1,1 +1,24 @@
+import { useSelector } from "react-redux";
+//This hook is used to access data (state) from your Redux store in a React component.
+
+function Navabar() {
+  const k = useSelector((store) => store.cart.k);
+  //cart is a slice name and cart ke andar k hai so k ki value access ho gyi aise.
+  console.log(k);
+  return (
+    <div>
+      Cart <span>{k}</span>
+    </div>
+  );
+}
+
+export default Navabar;
+
+//Explanation:
+
+// useSelector takes a function.
+// That function receives the entire Redux store state.
+// You're accessing the cart slice: store.cart
+// Inside that slice, you're reading the value of k (your cart item count).
+// So, k now holds the current cart count.
 
