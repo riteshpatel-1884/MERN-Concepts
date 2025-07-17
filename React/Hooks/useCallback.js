fd3a79a15b2bd00844b6720c3f774e1c635c4347
Ritesh Patel <129499361+riteshpatel-1884@users.noreply.github.com>
2025-07-17 21:46:24 +0530

    Update useCallback.js

--- React/Hooks/useCallback.js
+++ React/Hooks/useCallback.js
@@ -1,1 +1,25 @@
 
+✅ Hooks in React Router (v6+) (if you use React Router)
+useNavigate
+
+useParams
+
+useLocation
+
+useMatch
+
+useOutletContext
+
+useSearchParams
+
+✅ Hooks in Other Libraries (optional but common)
+📦 Redux Toolkit
+useSelector
+
+useDispatch
+
+🧪 React Testing Library
+renderHook
+
+act
+
