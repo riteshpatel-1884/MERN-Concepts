5ed64315dab107203100c009cdd3ef77dd25641d
Ritesh Patel <129499361+riteshpatel-1884@users.noreply.github.com>
2025-07-22 18:15:43 +0530

    Update app.js

--- Backend/Node/Basic Installation/app.js
+++ Backend/Node/Basic Installation/app.js
@@ -1,9 +1,7 @@
 const express = require("express");
-const cors = require("cors");
 require("dotenv").config();
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000
